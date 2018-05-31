1eef29d2ddf5f251ea7ab57af934496d5a136bff
Charles Hatch <charlesghatch@gmail.com>
2018-05-31 14:27:06 -0600

    completed

--- constructors.js
+++ constructors.js
@@ -15,6 +15,10 @@
 
 // Code here
 
+function CarFactory(make, model) {
+  this.make = make;
+  this.model = model;
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,6 +37,9 @@ function Employee(name, email, hireDate) {
 */
 
 // Code here
+
+let bob = new Employee('Bob', 'bob@gmail.com', '01-02-98')
+
 
 
 
@@ -54,5 +61,17 @@ mustang.moveCar(); // Increments mustang' move property by 10. Returns the new m
 */
 
 // Code here
+
+
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+
+  this.moveCar= function(){
+   return this.move +=10
+  }
+}
 
 
